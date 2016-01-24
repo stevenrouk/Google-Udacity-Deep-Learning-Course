5a485ca3ac59eafddc595f98e576e96a42c2fed1
Steven Rouk <stevenrouk@gmail.com>
2016-01-24 11:54:06 -0700

    Write softmax function and plot

--- lesson-1/softmax.py
+++ lesson-1/softmax.py
@@ -1,23 +1,58 @@
-import math
+"""Softmax."""
 
-# let's say these are the output of our logistic function
-scores = [3.0, 1.0, 0.3]
+scores = [3.0, 1.0, 0.2]
 
-def softmax(scores):
-    """INPUT: a list of scores that were output of a logistic function
-    OUTPUT: a list of probabilities for each of those scores, respectively"""
+import numpy as np
 
-    e = math.e
+# SOFTMAX FUNCTIONS
+def softmax_my_solution(x):
+    """My janky solution to the softmax problem."""
+    x = np.array(x)
 
-    # the denominator of each probability is the sum
-    # of e to the power of each score
-    denom = sum([e**score for score in scores])
+    if len(x.shape) == 1:
+        x.shape = x.shape[0], 1
 
-    probs = []
-    for score in score:
-        probs.append((e**score)/denom)
+    probabilities = np.ones_like(x)
 
-    return probs
+    for i in xrange(len(x[0])):
+        denom = sum([np.e**num for num in x[:,i]])
+
+        for j in xrange(len(x[:,i])):
+            probabilities[j,i] = (np.e**x[j,i]) / denom
+
+    if x.shape[1] == 1:
+        probabilities.shape = 1, probabilities.shape[0]
+        probabilities = probabilities[0]
+
+    return probabilities
+
+print softmax_my_solution(scores)
 
 
-print softmax(scores)
+def softmax_answer(x):
+    """The video's answer to the softmax function."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+print softmax_answer(scores)
+
+
+# PLOTTING SOFTMAX CURVES
+import matplotlib.pyplot as plt
+x = np.linspace(-2.0, 6.0, num=81)  # increases by 0.1, more consistent than np.arange()
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax_answer(scores).T, linewidth=2)
+
+
+# PLOT BAR CHART SUMS OF PROBABILITIES
+x_scores = softmax_answer(scores).T[:,0]
+one_scores = softmax_answer(scores).T[:,1]
+point_two_scores = softmax_answer(scores).T[:,2]
+
+width = 0.1
+
+p1 = plt.bar(x, x_scores, width, color='b', alpha=0.25)
+p2 = plt.bar(x, one_scores, width, color='r', bottom=x_scores, alpha=0.25)
+p2 = plt.bar(x, point_two_scores, width, color='g', bottom=x_scores+one_scores, alpha=0.25)
+
+plt.show()
